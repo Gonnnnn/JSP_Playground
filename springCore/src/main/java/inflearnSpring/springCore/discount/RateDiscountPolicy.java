2affedb20c3d135845f3c351ec738cb351a94ef3
Gonnnnn <geonkim@yonsei.ac.kr>
2022-04-09 15:28:36 +0900

    Implement RateDiscountPolicy and test code

---
+++ springCore/src/main/java/inflearnSpring/springCore/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package inflearnSpring.springCore.discount;
+
+import inflearnSpring.springCore.member.Grade;
+import inflearnSpring.springCore.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
